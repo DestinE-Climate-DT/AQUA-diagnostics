1ead27c4942a7ad536a5966723797ebb905eb713
Jost von Hardenberg <hardenberg@gmail.com>
2026-02-15 22:43:30 +0100

    fix old values

--- tests/test_ocean3d/test_stratification.py
+++ tests/test_ocean3d/test_stratification.py
@@ -30,4 +30,4 @@ def test_stratification():
     # assert strat.data["mld"].values == pytest.approx(25.49270658, rel=approx_rel)
     # assert strat.data["rho"].isel(level=1).values == pytest.approx(26.8719114, rel=approx_rel)
     assert strat.data["mld"].values == pytest.approx(2.5000076,rel=approx_rel)
-    assert strat.data["rho"].isel(level=1).values == pytest.approx(-3.34617212e+08,rel=approx_rel)
+    assert strat.data["rho"].isel(level=1).values == pytest.approx(-333897267.539,rel=approx_rel)
