c216774634111295e8e9b454e582ae1c3644bae4
MC-Corda <mariaclara.corda@polito.it>
2026-02-20 17:30:04 +0100

    fix in out file header

--- frontier-diagnostics/tropical_cyclones/tropical_cyclones/detect_nodes.py
+++ frontier-diagnostics/tropical_cyclones/tropical_cyclones/detect_nodes.py
@@ -228,17 +228,20 @@ class DetectNodes():
         tempest_fileout = os.path.join(
             self.paths['tmpdir'], 'tempest_output_' + timestep + '.txt')
         self.tempest_fileout = tempest_fileout
+        self.logger.warning(f'Tempest output file created:  {tempest_fileout}')
+
 
         # if the orography is found run stitch nodes accordingly
         if 'z' in self.lowres2d.data_vars or self.orography:
             self.logger.debug(f'Running DetectNodes with orography')
-            detect_string = f'DetectNodes --in_data {tempest_filein} --timefilter 6hr --out_header true --out {tempest_fileout} --out_file_format csv --searchbymin {tempest_dictionary["psl"]} ' \
+                        #detect_string = f'DetectNodes --in_data {tempest_filein} --timefilter 6hr --out_header --out {tempest_fileout} --out_file_format csv --searchbymin {tempest_dictionary["psl"]} ' \
+            detect_string = f'DetectNodes --in_data {tempest_filein} --timefilter 6hr --out_header --out {tempest_fileout}  --searchbymin {tempest_dictionary["psl"]} ' \
                 f'--closedcontourcmd {tempest_dictionary["psl"]},200.0,5.5,0;_DIFF({tempest_dictionary["zg"]}(30000Pa),{tempest_dictionary["zg"]}(50000Pa)),-58.8,6.5,1.0 --mergedist 6.0 ' \
                 f'--outputcmd {tempest_dictionary["psl"]},min,0;_VECMAG({tempest_dictionary["uas"]},{tempest_dictionary["vas"]}),max,2;{tempest_dictionary["orog"]},min,0 --latname {tempest_dictionary["lat"]} --lonname {tempest_dictionary["lon"]}'
 
         else:
             self.logger.debug(f'Running DetectNodes without orography')
-            detect_string = f'DetectNodes --in_data {tempest_filein} --timefilter 6hr --out_header true --out {tempest_fileout} --out_file_format csv --searchbymin {tempest_dictionary["psl"]} ' \
+            detect_string = f'DetectNodes --in_data {tempest_filein} --timefilter 6hr --out_header --out {tempest_fileout}  --searchbymin {tempest_dictionary["psl"]} ' \
                 f'--closedcontourcmd {tempest_dictionary["psl"]},200.0,5.5,0;_DIFF({tempest_dictionary["zg"]}(30000Pa),{tempest_dictionary["zg"]}(50000Pa)),-58.8,6.5,1.0 --mergedist 6.0 ' \
                 f'--outputcmd {tempest_dictionary["psl"]},min,0;_VECMAG({tempest_dictionary["uas"]},{tempest_dictionary["vas"]}),max,2 --latname {tempest_dictionary["lat"]} --lonname {tempest_dictionary["lon"]}'
 
