5746ce08d9d4dab93f42a6d153ec7596df405075
Jost von Hardenberg <jost.hardenberg@polito.it>
2026-03-02 00:28:43 +0100

    load in memory before saving

--- aqua/diagnostics/base/output_saver.py
+++ aqua/diagnostics/base/output_saver.py
@@ -238,6 +238,11 @@ class OutputSaver:
             }
 
         dataset.attrs.update(metadata)
+        
+        self.logger.debug(f"Loading data in memory")
+        dataset.load()
+        self.logger.debug(f"Writing to netcdf %s", filepath)
+        
         dataset.to_netcdf(filepath)
 
         # create catalog entry for netcdf file
