4d0bb2c4a14f738133ab22385180d75154c764fd
Emanuele Tovazzi <tovaz92@gmail.com>
2026-03-31 10:08:14 +0200

    manual fix (from: ruff check . --no-cache)

--- aqua/diagnostics/sshVariability/__init__.py
+++ aqua/diagnostics/sshVariability/__init__.py
@@ -6,4 +6,4 @@ from .plot_ssh_variability import SshVariabilityPlot
 from .ssh_variability import SshVariabilityCompute
 
 # This specifies which methods are exported publicly, used by "from ssh_class *"
-__all__ = ["SshVariabilityCompute", "sshVariablityPlot"]
+__all__ = ["SshVariabilityCompute", "SshVariabilityPlot"]
