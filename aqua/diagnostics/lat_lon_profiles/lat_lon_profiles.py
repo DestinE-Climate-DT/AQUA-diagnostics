2370e02d717cc6f73a952726bfbbc40728e12c90
Marco Cadau <marco.cadau@polito.it>
2026-04-13 11:32:53 +0200

    LatLonProfiles format fix

--- aqua/diagnostics/lat_lon_profiles/lat_lon_profiles.py
+++ aqua/diagnostics/lat_lon_profiles/lat_lon_profiles.py
@@ -16,9 +16,10 @@ class LatLonProfiles(Diagnostic):
 
     Supported Mean Types:
         - 'zonal': Average over longitude, producing latitude profiles
-        - 'meridional': Average over latitude, producing longitude profilesThe class evaluates a seasonal frequency and the entire period (longterm).
+        - 'meridional': Average over latitude, producing longitude profiles.
 
-	"""
+    """
+
     MINIMUM_MONTHS_REQUIRED = 12
 
     def __init__(
@@ -62,10 +63,18 @@ class LatLonProfiles(Diagnostic):
             loglevel (str, optional): The log level to be used for the logging.
         """
         # Initialize the Diagnostic class with the provided parameters
-        super().__init__(catalog=catalog, model=model, exp=exp, source=source, regrid=regrid,
-                         startdate=startdate, enddate=enddate,
-                         std_startdate=std_startdate, std_enddate=std_enddate,
-                         loglevel=loglevel)
+        super().__init__(
+            catalog=catalog,
+            model=model,
+            exp=exp,
+            source=source,
+            regrid=regrid,
+            startdate=startdate,
+            enddate=enddate,
+            std_startdate=std_startdate,
+            std_enddate=std_enddate,
+            loglevel=loglevel,
+        )
         self.diagnostic_name = diagnostic_name
 
         self.logger = log_configure(log_level=loglevel, log_name="LatLonProfiles")
@@ -117,17 +126,15 @@ class LatLonProfiles(Diagnostic):
         if formula:
             super().retrieve(reader_kwargs=reader_kwargs, months_required=self.MINIMUM_MONTHS_REQUIRED)
             self.logger.debug("Evaluating formula %s", var)
-            self.data = EvaluateFormula(data=self.data, formula=var, long_name=long_name,
-                                        short_name=standard_name, units=units,
-                                        loglevel=self.loglevel).evaluate()
+            self.data = EvaluateFormula(
+                data=self.data, formula=var, long_name=long_name, short_name=standard_name, units=units, loglevel=self.loglevel
+            ).evaluate()
             if self.data is None:
-                raise ValueError(f'Error evaluating formula {var}. '
-                                 'Check the variable names and the formula syntax.')
+                raise ValueError(f"Error evaluating formula {var}. Check the variable names and the formula syntax.")
         else:
             super().retrieve(var=var, reader_kwargs=reader_kwargs, months_required=self.MINIMUM_MONTHS_REQUIRED)
             if self.data is None:
-                raise ValueError(f'Variable {var} not found in the data. '
-                                 'Check the variable name and the data source.')
+                raise ValueError(f"Variable {var} not found in the data. Check the variable name and the data source.")
             # Get the xr.DataArray to be aligned with the formula code
             self.data = self.data[var]
 
