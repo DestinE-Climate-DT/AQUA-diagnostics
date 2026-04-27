94b901a8288e3234b60271dd8a72c716d22c55f1
mnurisso <m.nurisso@isac.cnr.it>
2026-04-27 11:45:42 +0200

    attributes for nao results

--- aqua/diagnostics/teleconnections/nao.py
+++ aqua/diagnostics/teleconnections/nao.py
@@ -132,6 +132,9 @@ class NAO(BaseMixin):
         # Drop NaNs
         indx = indx.dropna(dim="time")
 
+        indx.attrs["long_name"] = f"{self.telecname} index"
+        indx.attrs["months_window"] = months_window
+
         self.logger.debug("Index evaluated")
 
         # Save the index in the class
