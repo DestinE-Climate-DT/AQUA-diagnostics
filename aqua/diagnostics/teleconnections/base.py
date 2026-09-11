0b69d5b1fb58fccc9e3082a755b5732d4492c2b2
mnurisso <m.nurisso@isac.cnr.it>
2026-09-11 12:15:24 +0200

    Revert "teleconnections definitions shipped as python code to simplify the import"
    This reverts commit e9a3f3adef6631016ca58b00836d96b38587155f.

--- aqua/diagnostics/teleconnections/base.py
+++ aqua/diagnostics/teleconnections/base.py
@@ -1,12 +1,12 @@
+import os
 from typing import Union
 
 import xarray as xr
 
+from aqua.core.configurer import ConfigPath
 from aqua.core.logger import log_configure
-from aqua.core.util import convert_data_units, get_realizations, select_season, time_to_string, to_list
+from aqua.core.util import convert_data_units, get_realizations, load_yaml, select_season, time_to_string, to_list
 from aqua.diagnostics.base import SAVE_FORMAT, Diagnostic, OutputSaver, TitleBuilder, collapse_era5_duplicate
-
-from .definitions import ENSO_DEFINITIONS, MJO_DEFINITIONS, NAO_DEFINITIONS  # noqa: F401, needed for load_definition to work
 
 xr.set_options(keep_attrs=True)
 
@@ -23,6 +23,8 @@ class BaseMixin(Diagnostic):
         startdate: str = None,
         enddate: str = None,
         var: str = None,
+        configdir: str = None,
+        definition: str = "teleconnections-destine",
         loglevel: str = "WARNING",
     ):
         """
@@ -39,6 +41,9 @@ class BaseMixin(Diagnostic):
             enddate (str): The end date of the data to be retrieved.
                            If None, all available data will be retrieved.
             var (str): The variable to be used. If None, the variable will be determined by the definition.
+            configdir (str): The directory where the definition file is located.
+                             If None, the default directory will be used.
+            definition (str): The filename of the definition file. Default is 'teleconnections-destine'.
             loglevel (str): The log level to be used. Default is 'WARNING'.
         """
         super().__init__(
@@ -52,7 +57,7 @@ class BaseMixin(Diagnostic):
             loglevel=loglevel,
         )
 
-        self.definition = self.load_definition(telecname=telecname)
+        self.definition = self.load_definition(configdir=configdir, definition=definition, telecname=telecname)
         self.var = var or self.definition.get("field")
         self.telecname = telecname
 
@@ -132,19 +137,33 @@ class BaseMixin(Diagnostic):
 
         return data, index
 
-    def load_definition(self, telecname: str):
+    def load_definition(self, configdir: str = None, definition: str = "teleconnections-destine", telecname: str = None):
         """
         Load the definition for the teleconnections.
 
         Args:
+            configdir (str): The directory where the definition file is located.
+                              If None, the default directory will be used.
+            definition (str): The filename of the definition file.
+                             Default is 'teleconnections-destine'.
             telecname (str): The name of the teleconnection. It selects the subset of the definition.
 
         Returns:
-            dict: The definition dictionary.
+            dict: The definition file as a dictionary.
         """
-        definition_name = telecname.upper() + "_DEFINITIONS"
-        definitions = definition_name in globals() and globals()[definition_name] or None
-        return definitions
+        # Add yaml to definition if not present
+        if not definition.endswith(".yaml"):
+            definition = f"{definition}.yaml"
+        if not configdir:
+            configdir = ConfigPath().get_config_dir()
+            configdir = os.path.join(configdir, "tools", "teleconnections", "definitions")
+
+        definition_file = os.path.join(configdir, definition)
+        self.logger.debug(f"Loading definition file: {definition_file}")
+
+        definition_dict = load_yaml(definition_file)
+
+        return definition_dict[telecname] if telecname else definition_dict
 
 
 class PlotBaseMixin:
