4d0b50a41e7d5f38d82317f5ec4a16196e500e5b
Matteo Nurisso <m.nurisso@isac.cnr.it>
2026-09-15 10:44:58 +0300

    units configurable in plot_params

--- aqua/diagnostics/teleconnections/cli_teleconnections.py
+++ aqua/diagnostics/teleconnections/cli_teleconnections.py
@@ -90,8 +90,14 @@ def main(argv=None):
 
                     # Loop over each variable in statistics_var and each season to compute regressions and correlations.
                     for var in statistics_var:
+
+                        # Check in the plot_params if the variable has a specific unit defined, otherwise use None.
+                        var_units = None
+                        if "plot_params" in nao_config and var in nao_config["plot_params"]:
+                            var_units = nao_config["plot_params"][var].get("units", None)
+
                         for season in seasons:
-                            nao_regressions[var][season][i] = nao[i].compute_regression(var=var, season=season)
+                            nao_regressions[var][season][i] = nao[i].compute_regression(var=var, season=season, units=var_units)
                             nao_correlations[var][season][i] = nao[i].compute_correlation(var=var, season=season)
 
                             extra_keys = {"var": var, "season": season} if season != "annual" else {"var": var}
@@ -294,8 +300,14 @@ def main(argv=None):
 
                     # Loop over each variable in statistics_var and each season to compute regressions and correlations.
                     for var in statistics_var:
+
+                        # Check in the plot_params if the variable has a specific unit defined, otherwise use None.
+                        var_units = None
+                        if "plot_params" in enso_config and var in enso_config["plot_params"]:
+                            var_units = enso_config["plot_params"][var].get("units", None)
+
                         for season in seasons:
-                            enso_regressions[var][season][i] = enso[i].compute_regression(var=var, season=season)
+                            enso_regressions[var][season][i] = enso[i].compute_regression(var=var, season=season, units=var_units)
                             enso_correlations[var][season][i] = enso[i].compute_correlation(var=var, season=season)
 
                             extra_keys = {"var": var, "season": season} if season != "annual" else {"var": var}
